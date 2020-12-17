5a1cb7ada3f1ae8dd04e00223f6f15b7547940e0
Sabrina <sabrinacunsolo85@gmail.com>
2020-12-17 15:59:40 +0100

    aggiunto collegam a dischi php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
+<?php include 'dischi.php' ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
+        <!-- css -->
+        <link rel="stylesheet" href="dist/app.css">
+        <!-- fontawesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
         <title>Dischi - PHP</title>
     </head>
     <body>
@@ -9,7 +14,17 @@
             <h1>Dischi - PHP</h1>
         </header>
         <main>
-            
+            <div class="container">
+                <div class="container-dischi">
+                    <div class="card">
+                    <img src="" alt="poster">
+                    <h5 class="title">titolo</h5>
+                    <p class="author">artista</p>
+                    <p class="genre">genere</p>
+                    <p class="year">anno</p>
+                    </div>
+                </div>
+            </div>
         </main>
     </body>
 </html>
