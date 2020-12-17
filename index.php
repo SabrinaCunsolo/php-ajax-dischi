fbe5afa525a9aed0a08fa2d83b74e0cb84c21886
Sabrina <sabrinacunsolo85@gmail.com>
2020-12-17 15:10:37 +0100

    inizio esercizio php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Dischi - PHP</title>
+    </head>
+    <body>
+        <header>
+            <h1>Dischi - PHP</h1>
+        </header>
+        <main>
+            
+        </main>
+    </body>
+</html>
